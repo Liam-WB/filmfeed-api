6e8f1b5dc5ab4125ceb2c71f05aedf9675cd69bf
Liam-WB <>
2024-02-09 02:40:14 +0000

    -Add to profile model in profiles app

--- profiles/models.py
+++ profiles/models.py
@@ -2,4 +2,18 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-class Profile(models.Model)
+class Profile(models.Model):
+    owner = models.OneToOneField(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    name = models.CharField(max_length=255, blank=True)
+    content = models.TextField(blank=True)
+    image = models.ImageField(
+        upload_to='images/', default='../default_profile_qdjgyp'
+    )
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return f"{self.owner}'s profile"
