586008f9dd9263d43e5ab5a145533b2b68bea7e0
Liam-WB <>
2024-02-10 16:06:39 +0000

    -Add profiles urls, view

--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,8 @@
 from rest_framework.value import APIView
 from rest_framework.response import Response
 from .models import Profile
+
+class ProfileList(APIView):
+    def get(self, request):
+        profiles = Profile.objects.all()
+        return Response(profiles)
