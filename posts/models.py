26830b00d8f94f8ce02bc17c409665449c419a85
Liam-WB <>
2024-02-11 12:25:21 +0000

    -Add post model, owner, is_owner, profile_id, profile_image serializers -Add image_filter field to post model

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,42 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Post(models.Model):
+    """
+    Post model, related to 'owner', i.e. a User instance.
+    Default image set so that we can always reference image.url.
+    """
+    image_filter_choices = [
+        ('_1977', '1977'),
+        ('brannan', 'Brannan'),
+        ('earlybird', 'Earlybird'),
+        ('hudson', 'Hudson'),
+        ('inkwell', 'Inkwell'),
+        ('lofi', 'Lo-Fi'),
+        ('kelvin', 'Kelvin'),
+        ('normal', 'Normal'),
+        ('nashville', 'Nashville'),
+        ('rise', 'Rise'),
+        ('toaster', 'Toaster'),
+        ('valencia', 'Valencia'),
+        ('walden', 'Walden'),
+        ('xpro2', 'X-pro II')
+    ]
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    title = models.CharField(max_length=255)
+    content = models.TextField(blank=True)
+    image = models.ImageField(
+        upload_to='images/', default='../default_profile_qdjgyp', blank=True
+    )
+    image_filter = models.CharField(
+        max_length=32, choices=image_filter_choices, default='normal'
+    )
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return f'{self.id} {self.title}'
---
+++ posts/serializers.py
@@ -0,0 +1,20 @@
+from rest_framework import serializers
+from .models import Post
+
+
+class PostSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+    is_owner = serializers.SerializerMethodField()
+    profile_id = serializers.ReadOnlyField(source='owner.id')
+    profile_image = serializers.ReadOnlyField(source='owner.image.url')
+
+    def get_is_owner(self, obj):
+        request = self.context['request']
+        return request.user == obj.owner
+
+    class Meta:
+        model = Post
+        fields = [
+            'id', 'owner', 'is_owner', 'created_at', 'updated_at', 'title',
+            'content', 'image'
+        ]
