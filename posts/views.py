a69a09ad42af07b33b7a773ffba894c63f6a524a
Liam Blake <liamwartnerblake@gmail.com>
2024-02-13 23:11:23 +0000

    -Add PostList method in posts/views.py

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import status, permissions
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .models import Post
+from .serializers import PostSerializer
 
-# Create your views here.
+
+class PostList(APIView):
+    serializer_class = PostSerializer
+    permission_classes = [
+        permissions.IsAuthenticatedOrReadOnly
+    ]
+
+    def get(self, request):
+        posts = Post.objects.all()
+        serializer = PostSerializer(
+            posts, many=True, context={'request': request}
+        )
+        return Response(serializer.data)
