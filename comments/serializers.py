ca4f6dc71bf4e2990ef9049ea870ee73daf667d2
Liam Blake <liamwartnerblake@gmail.com>
2024-02-14 16:30:11 +0000

    -Add CommentDetailSerializer to comments app

--- comments/serializers.py
+++ comments/serializers.py
@@ -3,6 +3,10 @@ from .models import Comment
 
 
 class CommentSerializer(serializers.ModelSerializer):
+    """
+    Serializer for the Comment model
+    Adds three extra fields when returning a list of Comment instances
+    """
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
@@ -19,3 +23,11 @@ class CommentSerializer(serializers.ModelSerializer):
             'profile_image', 'post', 'created_at', 'updated_at',
             'content'
         ]
+
+
+class CommentDetailSerializer(CommentSerializer):
+    """
+    Serializer for the Comment model used in Detail view
+    Post is a read only field so that we dont have to set it on each update
+    """
+    post = serializers.ReadOnlyField(source='post.id')
