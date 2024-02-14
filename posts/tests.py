9fb12915fdc4453a03b6094176d34ae6f7606e8f
Liam Blake <liamwartnerblake@gmail.com>
2024-02-14 14:21:57 +0000

    -Add setUp and test_can_list_posts methods to posts/tests.py

--- posts/tests.py
+++ posts/tests.py
@@ -1,3 +1,17 @@
-from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Post
+from rest_framework import status
+from rest_framework import APITestCase
 
-# Create your tests here.
+
+class PostListViewTests(APITestCase):
+    def setUp(self):
+        User.objects.create_user(username='john', password='password')
+
+    def test_can_list_posts(self):
+        john = User.objects.get(username='john')
+        Post.objects.create(owner=john, title='a title')
+        response = self.client.get('/posts/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        print(response.data)
+        print(len(response.data))
