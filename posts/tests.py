49db3f29ba0eba6d9cfabf0cd12caa5218c93790
Liam Blake <liamwartnerblake@gmail.com>
2024-02-14 15:07:35 +0000

    -Add invalid id test, update tests to PostDetailViewTests

--- posts/tests.py
+++ posts/tests.py
@@ -43,3 +43,19 @@ class PostDetailViewTests(APITestCase):
         response = self.client.get('/posts/1/')
         self.assertEqual(response.data['title'], 'a title')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_cant_retrieve_post_using_invalid_id(self):
+        response = self.client.get('/posts/999/')
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_user_can_update_own_post(self):
+        self.client.login(username='john', password='password')
+        response = self.client.put('/posts/1/', {'title': 'a new title'})
+        post = Post.objects.filter(pk=1).first()
+        self.assertEqual(post.title, 'a new title')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_user_cant_update_another_users_post(self):
+        self.client.login(username='john', password='passsword')
+        response = self.client.put('/posts/2/', {'title': 'a new title'})
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
