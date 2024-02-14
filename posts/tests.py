1248d5048deee2335c53945f69316b5e36161e37
Liam Blake <liamwartnerblake@gmail.com>
2024-02-14 14:26:06 +0000

    -Add test_logged_in_user_can_create_post method -Fix import for APITestCase

--- posts/tests.py
+++ posts/tests.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.models import User
 from .models import Post
 from rest_framework import status
-from rest_framework import APITestCase
+from rest_framework.test import APITestCase
 
 
 class PostListViewTests(APITestCase):
@@ -15,3 +15,10 @@ class PostListViewTests(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         print(response.data)
         print(len(response.data))
+
+    def test_logged_in_user_can_create_post(self):
+        self.client.login(username='john', password='password')
+        response = self.client.post('/posts/', {'title': 'a title'})
+        count = Post.objects.count()
+        self.assertEqual(count, 1)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
