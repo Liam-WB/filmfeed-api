146baa8ebc33648fe0d9f1553005c398260df360
Liam Blake <liamwartnerblake@gmail.com>
2024-02-15 17:13:16 +0000

    -Add create method to likes serializer for validation error

--- likes/serializers.py
+++ likes/serializers.py
@@ -1,3 +1,4 @@
+from django.db import IntegrityError
 from rest_framework import serializers
 from likes.models import Like
 
@@ -10,3 +11,11 @@ class LikeSerializer(serializers.ModelSerializer):
         fields = [
             'id', 'owner', 'created_at', 'post'
         ]
+
+    def create(self, validated_data):
+        try:
+            return super().create(validated_data)
+        except IntegrityError:
+            raise serializers.ValidationError({
+                'detail': 'possible duplicate'
+            })
