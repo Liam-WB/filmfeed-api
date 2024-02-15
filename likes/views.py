bb585e925c6a14e527f37c424f987b4f1127713c
Liam Blake <liamwartnerblake@gmail.com>
2024-02-15 17:29:10 +0000

    -Add like list generic view -Connect likes urls

--- likes/views.py
+++ likes/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from filmfeed_api.permissions import IsOwnerOrReadOnly
+from likes.models import Like
+from likes.serializers import LikeSerializer
 
-# Create your views here.
+
+class LikeList(generics.ListCreateAPIView):
+    """
+    List likes or create a like if logged in.
+    """
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    serializer_class = LikeSerializer
+    queryset = Like.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
