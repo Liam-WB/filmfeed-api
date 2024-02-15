19ee716529a0c247e6674d0d03fff8b9e85538ce
Liam Blake <liamwartnerblake@gmail.com>
2024-02-15 17:31:11 +0000

    -Add like detail generic view, url

--- likes/views.py
+++ likes/views.py
@@ -14,3 +14,12 @@ class LikeList(generics.ListCreateAPIView):
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
+
+
+class LikeDetail(generics.RetrieveDestroyAPIView):
+    """
+    Retrieve a like or delete it by id if you own it.
+    """
+    permission_classes = [IsOwnerOrReadOnly]
+    serializer_class = LikeSerializer
+    queryset = Like.objects.all()
