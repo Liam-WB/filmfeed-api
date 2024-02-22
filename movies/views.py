4c9ced86a91922c7dc2075f2f0fb36b239b1971f
Liam Blake <liamwartnerblake@gmail.com>
2024-02-22 14:58:06 +0000

    -Add perform_create method to movies/views.py to add movies

--- movies/views.py
+++ movies/views.py
@@ -3,6 +3,7 @@ from .models import Movie
 from .serializers import MovieSerializer
 from rest_framework.pagination import PageNumberPagination
 from .serializers import MovieSerializer  
+from rest_framework.response import Response
 
 class MoviePagination(PageNumberPagination):
     page_size = 10
@@ -10,8 +11,17 @@ class MoviePagination(PageNumberPagination):
     max_page_size = 100
 
 
-class MovieList(generics.ListAPIView):
+class MovieList(generics.ListCreateAPIView):
     queryset = Movie.objects.all()
     serializer_class = MovieSerializer
     filter_backends = [filters.SearchFilter]
     search_fields = ['title']
+
+    def perform_create(self, serializer):
+        title = self.request.data.get('title')
+        if title:
+            movie = Movie(title=title)
+            movie.save()
+            serializer.instance = movie
+        else:
+            raise serializers.ValidationError("Title field is required.")
