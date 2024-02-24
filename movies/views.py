815eeab1b3fca3dc92eb2ea3ea437b36a3f1d0b5
Liam Blake <liamwartnerblake@gmail.com>
2024-02-24 14:11:44 +0000

    -Remove movies app page pagination

--- movies/views.py
+++ movies/views.py
@@ -1,14 +1,7 @@
 from rest_framework import generics, filters
 from .models import Movie
 from .serializers import MovieSerializer
-from rest_framework.pagination import PageNumberPagination
-from .serializers import MovieSerializer  
 from rest_framework.response import Response
-
-class MoviePagination(PageNumberPagination):
-    page_size = 10
-    page_size_query_param = 'page_size'
-    max_page_size = 100
 
 
 class MovieList(generics.ListCreateAPIView):
