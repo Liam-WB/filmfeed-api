38c0325878460133075e7a8b03677ca0d251f808
Liam Blake <liamwartnerblake@gmail.com>
2024-02-27 15:30:23 +0000

    -Change CLIENT_ORIGIN url formatting

--- filmfeed_api/settings.py
+++ filmfeed_api/settings.py
@@ -112,9 +112,11 @@ if 'CLIENT_ORIGIN' in os.environ:
     CORS_ALLOWED_ORIGINS = [
         os.environ.get('CLIENT_ORIGIN')
     ]
-else:
+
+if 'CLIENT_ORIGIN_DEV' in os.environ:
+    extracted_url = re.match(r'^.+-', os.environ.get('CLIENT_ORIGIN_DEV', ''), re.IGNORECASE).group(0)
     CORS_ALLOWED_ORIGIN_REGEXES = [
-        r"^https://.*\.gitpod\.io$",
+        rf"{extracted_url}(eu|us)\d+\w\.gitpod\.io$",
     ]
 
 CORS_ALLOW_CREDENTIALS = True
