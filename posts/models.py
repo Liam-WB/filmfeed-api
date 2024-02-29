8244928bd884b4a729b71779843b2e1abee7419c
Liam Blake <liamwartnerblake@gmail.com>
2024-02-29 15:59:39 +0000

    -Fix profile and post default image urls

--- posts/models.py
+++ posts/models.py
@@ -9,7 +9,7 @@ class Post(models.Model):
     title = models.CharField(max_length=255)
     content = models.TextField(blank=True)
     image = models.ImageField(
-        upload_to='images/', default='../default_post_rgq6aq', blank=True
+        upload_to='images/', default='../default_post_ygsaqt', blank=True
     )
 
     class Meta:
--- profiles/models.py
+++ profiles/models.py
@@ -10,7 +10,7 @@ class Profile(models.Model):
     name = models.CharField(max_length=255, blank=True)
     bio = models.TextField(blank=True)
     image = models.ImageField(
-        upload_to='images/', default='../default_profile_qdjgyp'
+        upload_to='images/', default='../default_profile_vlxux2'
     )
 
     class Meta:
