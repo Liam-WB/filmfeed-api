78c8ef4180500d2e1a0eb683ae128722f3714f81
Liam Blake <liamwartnerblake@gmail.com>
2024-05-01 14:29:50 +0000

    -Remove movie_data from read only fields

--- movies/serializers.py
+++ movies/serializers.py
@@ -5,7 +5,7 @@ class MovieSerializer(serializers.ModelSerializer):
     class Meta:
         model = Movie
         fields = ['id', 'title', 'movie_data']
-        read_only_fields = ['movie_data']
+        read_only_fields = []
 
     def create(self, validated_data):
         validated_data.pop('movie_data', None) # SO CAN'T BE MODIFIED - DELETE IF WANT TO MODIFY IN UPDATE
