87731cc08a2a7ad4e403d26c428401f4f1cd603f
Liam Blake <liamwartnerblake@gmail.com>
2024-05-02 08:14:27 +0000

    -Remove movies movie_data model null=True field method

--- movies/models.py
+++ movies/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Movie(models.Model):
     title = models.CharField(max_length=255)
-    movie_data = models.JSONField(null=True, blank=True)
+    movie_data = models.JSONField(blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
